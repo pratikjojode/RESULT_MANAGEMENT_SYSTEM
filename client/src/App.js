50ec9858d504878d5b6ea3797f824a0f1ba5eb59
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2024-09-26 20:30:43 +0530

    aa
    aa

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Result Management System</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
-}
+};
 
 export default App;
