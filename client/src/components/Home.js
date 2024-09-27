37a6b9fe7ccde011dfea27745c2301b912db7734
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2024-09-27 21:22:37 +0530

    aa
    aa

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout/Layout.js"; // Import the Layout component
-import "../../styles/Home.css";
+import ".././styles/Home.css";
 
 const HomePage = () => {
   return (
@@ -8,13 +8,20 @@ const HomePage = () => {
       {/* Left column content */}
       <div className="left-column">
         <h1>Welcome to Result Management System</h1>
-        <p>Manage students' attendance, project reviews, and LinkedIn posts all in one place.</p>
+        <p>
+          Manage students' attendance, project reviews, and LinkedIn posts all
+          in one place.
+        </p>
       </div>
 
       {/* Right column content */}
       <div className="right-column">
         <h1>Advantages of Result Management System</h1>
-        <p>The most significant advantage of Result Management System is that it can be used efficiently by both the admins(Teachers) as well as the students.</p>
+        <p>
+          The most significant advantage of Result Management System is that it
+          can be used efficiently by both the admins(Teachers) as well as the
+          students.
+        </p>
       </div>
     </Layout>
   );
